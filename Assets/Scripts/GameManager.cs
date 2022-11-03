079d8c5c40a603306433fb9f7c8327a285ad0364
wonbo <wonbo123@gmail.com>
2022-11-03 16:19:00 +0900

    add timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
     public int Score { get; set; }
+
+    public TextMeshProUGUI timerText = null;
+
+    private float timer = 60;
 
     // Start is called before the first frame update
     private void Awake()
@@ -22,6 +27,16 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        
+        timer -= Time.deltaTime;
+
+        timerText.text = timer.ToString("F2");
+
+        if (timer < 0f)
+        {
+            timerText.text = "0.00";
+
+            Time.timeScale = 0f;
+            // 게임오버
+        }
     }
 }
